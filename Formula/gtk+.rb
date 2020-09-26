d96a1f3e7c87bc7351baac040380d328346a915a
shibafu <shibafu528@gmail.com>
2020-09-26 18:07:09 +0900

    cairo, gtk2 for x11

---
+++ Formula/gtk+.rb
@@ -0,0 +1,166 @@
+class Gtkx < Formula
+  desc "GUI toolkit"
+  homepage "https://gtk.org/"
+  revision 3
+
+#  stable do
+#    url "https://download.gnome.org/sources/gtk+/2.24/gtk+-2.24.32.tar.xz"
+#    sha256 "b6c8a93ddda5eabe3bfee1eb39636c9a03d2a56c7b62828b359bf197943c582e"
+#  end
+
+#  livecheck do
+#    url :stable
+#    regex(/gtk\+[._-]v?(2\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+#  end
+
+#  bottle do
+#    rebuild 1
+#    sha256 "09e223d16f3e891d2a4184c66fecbf0a777e70e23680ac794dd8c44e4a63d5cf" => :catalina
+#    sha256 "cec64106c085533a58f8d436f029b2d7199a14cd15af9ece086814396ba48b0e" => :mojave
+#    sha256 "30ce8d0a4062200196f8d802ae75769d8e05d530c338619d290704c46a7d317b" => :high_sierra
+#    sha256 "a1324b85f6749111c3eb598c6d3ed231eaa8281b60fc2eb13d48a5f342da3efc" => :sierra
+#  end
+
+  head do
+    url "https://gitlab.gnome.org/GNOME/gtk.git", branch: "gtk-2-24"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gtk-doc" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
+  depends_on "atk"
+  depends_on "gdk-pixbuf"
+  depends_on "hicolor-icon-theme"
+  depends_on "pango"
+  depends_on :x11
+
+  # Patch to allow Eiffel Studio to run in Cocoa / non-X11 mode, as well as Freeciv's freeciv-gtk2 client
+  # See:
+  # - https://bugzilla.gnome.org/show_bug.cgi?id=757187
+  # referenced from
+  # - https://bugzilla.gnome.org/show_bug.cgi?id=557780
+  # - Homebrew/homebrew-games#278
+  patch do
+    url "https://bug757187.bugzilla-attachments.gnome.org/attachment.cgi?id=331173"
+    sha256 "ce5adf1a019ac7ed2a999efb65cfadeae50f5de8663638c7f765f8764aa7d931"
+  end
+
+  patch :DATA
+
+  def install
+    args = ["--disable-dependency-tracking",
+            "--disable-silent-rules",
+            "--prefix=#{prefix}",
+            "--enable-static",
+            "--disable-glibtest",
+            "--enable-introspection=yes",
+#            "--with-gdktarget=quartz",
+            "--disable-visibility"]
+
+    if build.head?
+      inreplace "autogen.sh", "libtoolize", "glibtoolize"
+      ENV["NOCONFIGURE"] = "yes"
+      system "./autogen.sh"
+    end
+    system "./configure", *args
+    system "make", "install"
+
+    inreplace bin/"gtk-builder-convert", %r{^#!/usr/bin/env python$}, "#!/usr/bin/python"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtk/gtk.h>
+
+      int main(int argc, char *argv[]) {
+        GtkWidget *label = gtk_label_new("Hello World!");
+        return 0;
+      }
+    EOS
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    harfbuzz = Formula["harfbuzz"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pixman = Formula["pixman"]
+    flags = %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
+      -I#{include}/gtk-2.0
+      -I#{libpng.opt_include}/libpng16
+      -I#{lib}/gtk-2.0/include
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lgdk-quartz-2.0
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-quartz-2.0
+      -lintl
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
+
+__END__
+diff --git a/gdk/x11/gdkapplaunchcontext-x11.c b/gdk/x11/gdkapplaunchcontext-x11.c
+index 8796cc3..32e6dfe 100644
+Original from https://github.com/GNOME/gtk/commit/b366d6c5ffecbc1e786fc4722ca6049e28178f50
+--- a/gdk/x11/gdkapplaunchcontext-x11.c
++++ b/gdk/x11/gdkapplaunchcontext-x11.c
+@@ -26,7 +26,9 @@
+ #include <unistd.h>
+ 
+ #include <glib.h>
++#if defined(HAVE_GIO_UNIX) && !defined(__APPLE__)
+ #include <gio/gdesktopappinfo.h>
++#endif
+ 
+ #include "gdkx.h"
+ #include "gdkapplaunchcontext.h"
+@@ -363,10 +365,15 @@ _gdk_windowing_get_startup_notify_id (GAppLaunchContext *context,
+   else
+     workspace_str = NULL;
+ 
++#if defined(HAVE_GIO_UNIX) && !defined(__APPLE__)
+   if (G_IS_DESKTOP_APP_INFO (info))
+     application_id = g_desktop_app_info_get_filename (G_DESKTOP_APP_INFO (info));
+   else
+     application_id = NULL;
++#else
++  application_id = NULL;
++#warning Please add support for creating AppInfo from id for your OS
++#endif
+ 
+   startup_id = g_strdup_printf ("%s-%lu-%s-%s-%d_TIME%lu",
+                                 g_get_prgname (),
