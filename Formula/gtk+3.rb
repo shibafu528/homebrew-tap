2a5c3731415ab8fe6b1c558d43d401d799320bae
shibafu <shibafu528@gmail.com>
2021-12-25 18:50:06 +0900

    gtk3.24.31

---
+++ Formula/gtk+3.rb
@@ -0,0 +1,163 @@
+class Gtkx3 < Formula
+  desc "Toolkit for creating graphical user interfaces"
+  homepage "https://gtk.org/"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.31.tar.xz"
+  sha256 "423c3e7fdb4c459ee889e35fd4d71fd2623562541c1041b11c07e5ad1ff10bf9"
+  license "LGPL-2.0-or-later"
+
+  livecheck do
+    url :stable
+    regex(/gtk\+[._-](3 \.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+  end
+
+  depends_on "docbook" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "atk"
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "gsettings-desktop-schemas"
+  depends_on "hicolor-icon-theme"
+  depends_on "libepoxy"
+  depends_on "pango"
+
+  uses_from_macos "libxslt" => :build # for xsltproc
+
+  on_linux do
+    depends_on "cmake" => :build
+    depends_on "at-spi2-atk"
+    depends_on "cairo"
+    depends_on "iso-codes"
+    depends_on "libxkbcommon"
+    depends_on "xorgproto"
+    depends_on "wayland-protocols"
+  end
+
+  patch :DATA
+
+  def install
+    args = std_meson_args + %w[
+      -Dgtk_doc=false
+      -Dman=true
+      -Dintrospection=true
+    ]
+
+    if OS.mac?
+      args << "-Dquartz_backend=true"
+      args << "-Dx11_backend=false"
+    end
+
+    # ensure that we don't run the meson post install script
+    ENV["DESTDIR"] = "/"
+
+    # Find our docbook catalog
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    mkdir "build" do
+      system "meson", *args, ".."
+      system "ninja", "-v"
+      system "ninja", "install", "-v"
+    end
+
+    # Prevent a conflict between this and Gtk+2
+    mv bin/"gtk-update-icon-cache", bin/"gtk3-update-icon-cache"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system bin/"gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    system "#{bin}/gtk-query-immodules-3.0 > #{HOMEBREW_PREFIX}/lib/gtk-3.0/3.0.0/immodules.cache"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtk/gtk.h>
+
+      int main(int argc, char *argv[]) {
+        gtk_disable_setlocale();
+        return 0;
+      }
+    EOS
+    atk = Formula["atk"]
+    cairo = Formula["cairo"]
+    fontconfig = Formula["fontconfig"]
+    freetype = Formula["freetype"]
+    gdk_pixbuf = Formula["gdk-pixbuf"]
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    harfbuzz = Formula["harfbuzz"]
+    libepoxy = Formula["libepoxy"]
+    libpng = Formula["libpng"]
+    pango = Formula["pango"]
+    pixman = Formula["pixman"]
+    flags = %W[
+      -I#{atk.opt_include}/atk-1.0
+      -I#{cairo.opt_include}/cairo
+      -I#{fontconfig.opt_include}
+      -I#{freetype.opt_include}/freetype2
+      -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/gio-unix-2.0/
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{harfbuzz.opt_include}/harfbuzz
+      -I#{include}
+      -I#{include}/gtk-3.0
+      -I#{libepoxy.opt_include}
+      -I#{libpng.opt_include}/libpng16
+      -I#{pango.opt_include}/pango-1.0
+      -I#{pixman.opt_include}/pixman-1
+      -D_REENTRANT
+      -L#{atk.opt_lib}
+      -L#{cairo.opt_lib}
+      -L#{gdk_pixbuf.opt_lib}
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -L#{pango.opt_lib}
+      -latk-1.0
+      -lcairo
+      -lcairo-gobject
+      -lgdk-3
+      -lgdk_pixbuf-2.0
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lgtk-3
+      -lpango-1.0
+      -lpangocairo-1.0
+    ]
+    on_macos do
+      flags << "-lintl"
+    end
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+    # include a version check for the pkg-config files
+    assert_match version.to_s, shell_output("cat #{lib}/pkgconfig/gtk+-3.0.pc").strip
+  end
+end
+
+__END__
+diff --git a/gdk/quartz/meson.build b/gdk/quartz/meson.build
+index 70d688e1a7..3e0d76ddb6 100644
+--- a/gdk/quartz/meson.build
++++ b/gdk/quartz/meson.build
+@@ -44,12 +44,13 @@ gdk_quartz_public_headers = files(
+ install_headers(gdk_quartz_public_headers, subdir: 'gtk-3.0/gdk/quartz')
+ install_headers('gdkquartz.h', subdir: 'gtk-3.0/gdk')
+
++quartz_core_dep = dependency('appleframeworks', modules : 'QuartzCore', required : true)
+ core_graphics_dep = dependency('appleframeworks', modules : 'CoreGraphics', required : true)
+ appkit_dep = dependency('appleframeworks', modules : 'AppKit', required : true)
+ cocoa_dep = dependency('appleframeworks', modules : 'Cocoa', required : true)
+ carbon_dep = dependency('appleframeworks', modules : 'Carbon', required : true)
+
+-gdk_quartz_deps = [ core_graphics_dep, appkit_dep, cocoa_dep, carbon_dep ]
++gdk_quartz_deps = [ quartz_core_dep, core_graphics_dep, appkit_dep, cocoa_dep, carbon_dep ]
+
+ libgdk_quartz = static_library('gdk-quartz',
+   gdk_quartz_sources, gdkconfig, gdkenum_h,
+
